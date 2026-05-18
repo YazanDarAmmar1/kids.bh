7de358ec9f3b24bbe4398a36028f922d1c31cf44
yazanDarAmmar1 <darammary5@gmail.com>
2026-05-18 15:58:09 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     @livewireStyles
     @stack('head')
-    <link rel="stylesheet" href="{{ asset('home-assets/styles.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('home-assets/style.css') }}"/>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 </head>
